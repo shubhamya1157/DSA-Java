d9d9d56c9e6ef3a10c2c50ab4df5f8c88d35b958
Shubham Yadav <shubhamya.1157@gmail.com>
2026-01-25 11:53:14 +0530

    Add PushAtBottom method to MyStack class
    Implemented a method to push an element at the bottom of a stack.

---
+++ Stack/MyStack.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+public class MyStack{
+
+    public static void PushAtBottom(int val, Stack <Integer> st ){
+
+    if(st.size() == 0){
+     st.push(val);
+     return;
+    }
+    
+        int ele =  st.pop();
+        PushAtBottom(val , st);
+        st.push(ele);
+  
+
+    }
+ 
+    public static void main(String[] args){
+   
+   Stack <Integer> st1 = new Stack<>();
+
+    st1.push(10); // Pushing 10 into the stack
+    st1.push(20); // Pushing 20 into the stack
+    st1.push(30); // Pushing 30 into the stack
+    st1.push(40); // Pushing 40 into the stack
+    st1.push(50); // Pushing 50 into the stack
+
+    System.out.println(st1);
+    PushAtBottom(0,st1);
+    System.out.print(st1);
+
+    }
+}
