9a4b18528946cb9603a424bf7a7e2f4c8841407a
Shubham Yadav <shubhamya.1157@gmail.com>
2026-01-30 01:11:44 +0530

    Time: 438 ms (5.04%), Space: 48 MB (14.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,10 +1,3 @@
-//GFG question
-// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-// Note that you must do this in-place without making a copy of the arra
-
-//Using bubble sort approch
-
 class Solution {
     public void moveZeroes(int[] nums) {
         for(int i = 0; i < nums.length ; i++){
